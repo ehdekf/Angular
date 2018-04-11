f45d758bf3aa76bb282bd2115d7125469dd6d530
Kimdc <ehdekf89@naver.com>
2018-04-11 13:27:56 +0900

    Angular add Files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+//component
+import {FooterComponent} from './page/footer';
+import {HeaderComponent} from './page/header';
+import {DashboardComponent} from './page/dashboard/dashboard.component';
+import {BoardComponent} from './page/board/board.component';
+
+const appRoutes: Routes = [
+    { path: 'dashboard', component:  DashboardComponent},
+    { path: 'board', component:BoardComponent}
+];
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        BoardComponent,
+        FooterComponent,
+        HeaderComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(
+            appRoutes,
+            //{ enableTracing: true } // <-- debugging purposes only
+        ),
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/page/footer.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'footer-contnent',
+    templateUrl: '/footer.html',
+    //styleUrls: ['./app.component.css']
+})
+export class FooterComponent {
+    title = 'app';
+}
